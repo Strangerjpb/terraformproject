ef0d695a80dc610a8ce235b1140b2075f13a3bf4
Strangerjpb <133494238+Strangerjpb@users.noreply.github.com>
2023-06-15 15:37:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIATTAU7W3M74WQ2CRB"
-secret_key = "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"
+access_key = "AKIAYDGTNESDHK4KFSM4"
+secret_key = "ym4coOXO6AONq9u/kuAJA1u9E6bjNIWB86sKUkqs"
 }
 
 resource "aws_instance" "ec2_example" {
-  ami                    = "ami-0cca134ec43cf708f"
+  ami                    = "ami-03cb1380eec7cc118"
   instance_type          = var.instance_type
   tags = {
-    Name = "Terraform EC2"
+    Name = "Terra-server"
   }
 }
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "ap-south-1a"
-  size              = 40
+  size              = 20
 
   tags = {
     Name = "HelloWorld"
